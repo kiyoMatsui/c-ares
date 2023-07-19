bb0255eb23b24609e953643dbdd1f6eeacb9e377
kiyoMatsui <kiyo.matsui@gmx.com>
2023-07-19 21:49:04 +0100

    fix cmake 3

--- test/ares-test-live.cc
+++ test/ares-test-live.cc
@@ -157,18 +157,7 @@ VIRT_NONVIRT_TEST_F(DefaultChannelTest, LiveGetHostByNameFile) {
   }
 }
 
-TEST_P(DefaultChannelModeTest, LiveGetLocalhostByNameV4) {
-  HostResult result;
-  ares_gethostbyname(channel_, "localhost", AF_INET, HostCallback, &result);
-  Process();
-  EXPECT_TRUE(result.done_);
-  if (result.status_ != ARES_ECONNREFUSED) {
-    EXPECT_EQ(ARES_SUCCESS, result.status_);
-    EXPECT_EQ(1, (int)result.host_.addrs_.size());
-    EXPECT_EQ(AF_INET, result.host_.addrtype_);
-    EXPECT_NE(std::string::npos, result.host_.name_.find("localhost"));
-  }
-}
+
 
 TEST_P(DefaultChannelModeTest, LiveGetLocalhostByNameV6) {
   HostResult result;
